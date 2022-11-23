1fca27613e636d911d2045c28a8d8ada9bbed585
Kunalsarkar404 <vjkunal00@gmail.com>
2022-11-23 15:30:20 +0530

    AWS beanstalk initial commit

---
+++ src/test/java/com/ankercloud/aws/AwsPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankercloud.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
